a6f2d7709f49f2e59e660fd0a37dd7584773b30c
shiyuwudi <289052669@qq.com>
2017-11-05 10:08:04 +0800

    加载css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
 
     // lodash由本地加载
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,17 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
