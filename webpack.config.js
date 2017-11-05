aada8686e2a20af1d72be107393c106165399fd1
shiyuwudi <289052669@qq.com>
2017-11-05 08:56:00 +0800

    新建webpack配置文件

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// __dirname: 项目根目录 '/Users/shiyu/Desktop/webpack-demo'
+// path.resolve 拼接路径
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
