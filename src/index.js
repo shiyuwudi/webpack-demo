2df26cb262cf76afac47918dd39784e6fb541edb
shiyuwudi <289052669@qq.com>
2017-11-05 08:15:30 +0800

    本地依赖lodash

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
     var element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
+    // lodash由本地加载
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
