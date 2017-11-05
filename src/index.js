5e96f43464002d3189d23a335bfd665a053fadf4
shiyuwudi <289052669@qq.com>
2017-11-05 16:03:07 +0800

    html插件自动引入脚本

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
     var element = document.createElement('div');
+    var button = document.createElement('button');
+    button.innerHTML = "点我然后看控制台";
+    button.onclick = printMe;
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(button);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // __dirname: 项目根目录 '/Users/shiyu/Desktop/webpack-demo'
 // path.resolve 拼接路径
 
 module.exports = {
-    entry: './src/index.js',
+    // 多入口
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '多个output'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
