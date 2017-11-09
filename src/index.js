4e54298f889dfc8a500232cd8607dc2d904274cc
shiyuwudi <289052669@qq.com>
2017-11-09 21:40:27 +0800

    提取公共代码

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import { cube } from './math.js';
+import _ from 'lodash';
 
-if (process.env.NODE_ENV !== 'production') {
-    console.log('注意：当前不是生产环境!');
-}
-
-function component() {
-    var element = document.createElement('pre');
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
-    return element;
-}
-
-document.body.appendChild(component());
+console.log(_.join(['Index', 'module', 'loaded!'], ' '))
