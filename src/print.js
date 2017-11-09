7e7b35ef9de664c4b1ec24dbff11846c7b64ccd8
shiyuwudi <289052669@qq.com>
2017-11-10 00:50:26 +0800

    按需加载

--- src/print.js
+++ src/print.js
@@ -1,27 +1,5 @@
+console.log('print.js 模块已加载! 速速查看网络请求...');
 
-function loadXMLDoc() {
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState===4 && xmlhttp.status===200)
-        {
-            console.log('response', xmlhttp.responseText);
-            // document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET","/douban",true);
-    xmlhttp.send();
-}
-
-export default function printMe() {
-    console.log('222333!');
-    // loadXMLDoc();
+export default () => {
+  console.log('按钮被点击: 字数补丁!');
 }
